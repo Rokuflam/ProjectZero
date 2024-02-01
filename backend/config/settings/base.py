0603f2f030ce907d1faee01ea736fbda5d2f186d
Roman <98953084+Rokuflam@users.noreply.github.com>
2024-02-01 21:19:23 +0200

    Fixed entrypoint.sh | Added whitenoise for  mediafiles in production mode | added statick/media root

--- backend/config/settings/base.py
+++ backend/config/settings/base.py
@@ -53,6 +53,8 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+
+    'whitenoise.middleware.WhiteNoiseMiddleware',
 ]
 
 ROOT_URLCONF = 'config.urls'
@@ -124,7 +126,11 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = '/static/static/'
+MEDIA_URL = '/static/media/'
+
+MEDIA_ROOT = '/vol/web/media'
+STATIC_ROOT = '/vol/web/static'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
--- scripts/entrypoint.sh
+++ scripts/entrypoint.sh
@@ -1,9 +1,9 @@
-#!/bin/bash
+#!/bin/sh
 
 set -e
 
 # Set the default value for DJANGO_SETTINGS_MODULE
-DJANGO_SETTINGS_MODULE=${DJANGO_SETTINGS_MODULE:-src.settings.development}
+DJANGO_SETTINGS_MODULE=${DJANGO_SETTINGS_MODULE:-config.settings.development}
 
 # Run Django management commands with the specified settings
 python manage.py makemigrations --settings="$DJANGO_SETTINGS_MODULE" --no-input
@@ -11,4 +11,4 @@ python manage.py migrate --settings="$DJANGO_SETTINGS_MODULE" --no-input
 python manage.py collectstatic --settings="$DJANGO_SETTINGS_MODULE" --no-input
 
 # Run Gunicorn with the specified DJANGO_SETTINGS_MODULE
-exec gunicorn --env DJANGO_SETTINGS_MODULE="$DJANGO_SETTINGS_MODULE" src.wsgi:application -b 0.0.0.0:8080 --reload
+exec gunicorn --env DJANGO_SETTINGS_MODULE="$DJANGO_SETTINGS_MODULE" config.wsgi:application -b 0.0.0.0:8000 --reload
