9564a3c64e36b4df8c3484ffcf087480a5d0ac2d
Roman <98953084+Rokuflam@users.noreply.github.com>
2024-08-06 10:34:23 +0300

    updated main.tf

--- terraform/dev/main.tf
+++ terraform/dev/main.tf
@@ -26,7 +26,7 @@ resource "aws_security_group" "sg_ssh" {
     from_port   = 22
     to_port     = 22
     protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/32"]
+    cidr_blocks = ["0.0.0.0/0"]
   }
 
   ingress {
