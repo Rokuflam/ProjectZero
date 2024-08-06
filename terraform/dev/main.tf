01b53ef2a479730ee5a031a0484323bfa18ffb2c
Roman <98953084+Rokuflam@users.noreply.github.com>
2024-08-06 11:03:10 +0300

    Update main.tf

--- terraform/dev/main.tf
+++ terraform/dev/main.tf
@@ -35,6 +35,20 @@ resource "aws_security_group" "sg_ssh" {
     protocol    = "-1"
     cidr_blocks = ["0.0.0.0/0"]
   }
+
+  ingress {
+    from_port   = 443
+    to_port     = 443
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
 }
 
 resource "aws_eip" "dev_eip" {
