6848aec3a4d4676f34bffc6869030647f28d5785
Roman <98953084+Rokuflam@users.noreply.github.com>
2024-08-06 11:56:46 +0300

    added tcp 80 for http

--- terraform/dev/main.tf
+++ terraform/dev/main.tf
@@ -29,10 +29,10 @@ resource "aws_security_group" "sg_ssh" {
     cidr_blocks = ["0.0.0.0/0"]
   }
 
-  egress {
-    from_port   = 0
-    to_port     = 0
-    protocol    = "-1"
+  ingress {
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
     cidr_blocks = ["0.0.0.0/0"]
   }
 
